b221257af7f91774907db705d13b59e1db4f85b1
ASMR DEVELOPER <141846914+asmr-developer777@users.noreply.github.com>
2023-08-16 22:00:25 +0200

    + added project files for part 2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // to remove all non-whitelisted properties
 
   const config = new DocumentBuilder()
     .setTitle('Developer Blog')
